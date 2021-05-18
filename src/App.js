6fc8eeb0822364cc120cdb54d2e4e8a8c58445e5
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-05-18 19:24:18 -0400

    finish the project

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
     <div className='App'>
       <h1>Counter {counter} </h1>
       {isLogged ? <h3>Valuable information I shouldn't see</h3> : ''}
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(3))}>-</button>
     </div>
   );
 };
