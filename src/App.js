b777ce477a2ff6e3f597b9e2f204b7f9fd5e8859
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-05-18 19:14:42 -0400

    add actions

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment, decrement} from './actions/index';
+
 
 const App = () => {
   const counter = useSelector((state) => state.counterReducer);
   const isLogged = useSelector((state) => state.loggedReducer);
+  const dispatch = useDispatch();
+
   return (
     <div className='App'>
       <h1>Counter {counter} </h1>
-      <h3>Valuable information I shouldn't see</h3>
-      <h4>{isLogged}</h4>
+      {isLogged ? <h3> 
+      Valuable information I shouldn't see
+       </h3>  : '' }
+       <button onClick={() => dispatch(increment())}>+</button>
+       <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 };
