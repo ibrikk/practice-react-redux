e017d0665c7c2ca0ff188b314a771b6867242d27
Ibrahim Khalilov <ibrahimkhalilov@Ibrahims-Mac-mini.local>
2021-05-18 19:04:32 -0400

    add provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
-
+import { useSelector } from 'react-redux';
 
 const App = () => {
-  const counter = useSelector(state => state.counterReducer);
-  const isLogged = useSelector(state => state.loggedReducer)
+  const counter = useSelector((state) => state.counterReducer);
+  const isLogged = useSelector((state) => state.loggedReducer);
   return (
-    <div className="App">
-    <h1>Counter {counter} </h1>
-    <h3>Valuable information I shouldn't see</h3>
-    <h4>{isLogged}</h4>
+    <div className='App'>
+      <h1>Counter {counter} </h1>
+      <h3>Valuable information I shouldn't see</h3>
+      <h4>{isLogged}</h4>
     </div>
   );
-}
+};
 
 export default App;
